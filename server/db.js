45cd857dca880e2dcfc1a2cd2375288749c03e02
Raihan Mahmud <raihan_uits@outlook.com>
2023-11-24 02:20:26 +0600

    connecting database

---
+++ server/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+var mongoURL = 'mongodb+srv://admin:root@cluster0.iugfw.mongodb.net/'
+
+mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true })
+
+var connection = mongoose.connection
+
+connection.on('error', () => {
+    console.log('Database connection failed')
+})
+
+connection.on('connected', () => {
+    console.log('Database connected successfully')
+})
+
+module.exports = mongoose
