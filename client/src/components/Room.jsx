0b54c5c8cff0e224b3ff923942602cd62bc5b07e
Raihan Mahmud <raihan_uits@outlook.com>
2023-11-25 19:55:41 +0600

    room component

---
+++ client/src/components/Room.jsx
@@ -0,0 +1,7 @@
+export const Room = ({ room }) => {
+  return (
+    <div>
+        <h3>{room.name}</h3>
+    </div>
+  )
+}
--- client/src/screens/HomesScreen.jsx
+++ client/src/screens/HomesScreen.jsx
@@ -1,23 +1,31 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
+import { Room } from "../components/Room"
 
 export const HomeScreen = () => {
     const [rooms, setRooms] = useState([])
+    const [loading, setLoading] = useState()
+    const [error, setError] = useState()
     useEffect(() => {
         const fetchData = async () => {
             try {
+                setLoading(true)
                 const data = (await axios.get('/api/rooms/getallrooms')).data
                 setRooms(data)
+                setLoading(false)
             } catch (error) {
+                setError(true)
                 console.log(error)
+                setLoading(false)
             }
         }
         fetchData()
     }, [])
     return (
-        <>
-            <h1>Home Screen</h1>
-            <h4>There are {rooms.length} rooms</h4>
-        </>
+        <div className="row">
+            {loading ? (<h2>Loading...</h2>) : error ? (<h2>Error</h2>) : (rooms.map(room => {
+                return <div className="col-md-9"><Room room = {room} /></div>
+            }))}
+        </div>
     )
 }
