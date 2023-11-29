e622dfa08f60ddd165bddab09ccee2527a1314b3
Raihan Mahmud <raihan_uits@outlook.com>
2023-11-30 03:20:22 +0600

    filtering

--- client/src/screens/HomesScreen.jsx
+++ client/src/screens/HomesScreen.jsx
@@ -14,13 +14,15 @@ export const HomeScreen = () => {
     const [error, setError] = useState()
     const [checkIn, setCheckIn] = useState()
     const [checkOut, setCheckOut] = useState()
-    
+    const [duplicateRooms, setDuplicateRooms] = useState([])
+
     useEffect(() => {
         const fetchData = async () => {
             try {
                 setLoading(true)
                 const data = (await axios.get('/api/rooms/getallrooms')).data
                 setRooms(data)
+                setDuplicateRooms(data)
                 setLoading(false)
             } catch (error) {
                 setError(true)
@@ -30,17 +32,44 @@ export const HomeScreen = () => {
         }
         fetchData()
     }, [])
-    
+
     const filterByDate = dates => {
         setCheckIn(dates[0].format('DD-MM-YYYY'))
         setCheckOut(dates[1].format('DD-MM-YYYY'))
+
+        let temp = []
+        for (const room of duplicateRooms) {
+            let availability = false
+            if (room.currentBookings.length > 0) {
+                for (const booking of room.currentBookings) {
+                    if (!moment(moment(dates[0]).format('DD-MM-YYYY')).isBetween(
+                        booking.checkIn, booking.checkOut) &&
+                        !moment(moment(dates[1]).format('DD-MM-YYYY')).isBetween(
+                            booking.checkIn, booking.checkOut)
+                        
+                    ) {
+                        if (
+                            dates[0].format('DD-MM-YYYY') !== booking.checkIn &&
+                            dates[0].format('DD-MM-YYYY') !== booking.checkOut &&
+                            dates[1].format('DD-MM-YYYY') !== booking.checkIn &&
+                            dates[1].format('DD-MM-YYYY') !== booking.checkOut
+                        ) { availability = true }
+                    }
+                }
+            } else { availability = true }
+            if (availability === true) {
+                temp.push(room)
+            }
+        }
+        setRooms(temp)
+        console.log(temp)
     }
 
     return (
         <div className="container">
             <div className="row mt-5">
                 <div className="col-md-3">
-                    <RangePicker format = 'DD-MM-YYYY' onChange={filterByDate} />
+                    <RangePicker format='DD-MM-YYYY' onChange={filterByDate} />
                 </div>
             </div>
             <div className="row justify-content-center mt-5">
--- server/db.js
+++ server/db.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose')
 
 var mongoURL = 'mongodb+srv://admin:root@cluster0.iugfw.mongodb.net/sheyrooms'
 
+// stripe - client: pk_test_51OHpwDAQGIQYXRjhC0POSJmj8TZeI2gYYfjaT5519G3FVwZTPZgtkzvynRuKXe7DNGYs2cAjMyNVsDqJUlHtlaob009k9O137T
+// stripe - server: sk_test_51OHpwDAQGIQYXRjhU8d2ScTIOlO7r0QVb7WRVYAZOls49h7gDijeyL3CBKb0MlXkVBPhyffpHwdI6cqbAVP2Dme300e33VMM48
+
 mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true })
 
 var connection = mongoose.connection
