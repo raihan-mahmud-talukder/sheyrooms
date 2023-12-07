492143b213907d23f2504aea86751a4a2b84d51f
Raihan Mahmud <raihan_uits@outlook.com>
2023-12-07 15:19:20 +0600

    bookings style

--- client/src/screens/ProfileScreen.jsx
+++ client/src/screens/ProfileScreen.jsx
@@ -1,6 +1,7 @@
 import { Tabs } from "antd"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from 'axios'
+import { Loader } from "../components/Loader"
 const { TabPane } = Tabs
 const user = JSON.parse(localStorage.getItem('currentUser'))
 
@@ -26,18 +27,45 @@ export const ProfileScreen = () => {
 }
 
 export const MyBooking = () => {
+  const [bookings, setBookings] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState()
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const rooms = (await axios.post('/api/bookings/getbookingsbyuserid', { userId: user._id })).data
-        console.log(rooms)
-      } catch (error) { console.log(error) }
+        setLoading(true)
+        const data = (await axios.post('/api/bookings/getbookingsbyuserid', { userId: user._id })).data
+        console.log(data)
+        setBookings(data)
+        setLoading(false)
+      } catch (error) {
+        console.log(error)
+        setLoading(false)
+        setError(true)
+      }
     }
     fetchData()
-  })
+  }, [])
   return (
-    <div>
-      <h3>My Bookings</h3>
+    <div className="row">
+      <h3>Bookings</h3>
+      <div className="col-md-6">
+        {loading && <Loader />}
+        {bookings && bookings.map(booking => {
+          return (
+            <div className="booking">
+              <h4>{booking.room}</h4>
+              <p><b>Booking Id:</b> {booking._id}</p>
+              <p><b>Check In:</b> {booking.checkIn}</p>
+              <p><b>Check Out:</b> {booking.checkOut}</p>
+              <p><b>Amount:</b> {booking.amount}</p>
+              <p><b>Status:</b> {booking.status == 'booked' ? 'CONFIRMED' : 'cANCELLED'}</p>
+              <button className="btn btn-primary">Cancel Booking</button>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
