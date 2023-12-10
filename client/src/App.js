926080cc9bb3d191bc27ea26794ce4d803b4e133
Raihan Mahmud <raihan_uits@outlook.com>
2023-12-10 08:04:04 +0600

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,20 @@ import { LoginScreen } from './screens/LoginScreen'
 import { RegisterScreen } from './screens/RegisterScreen'
 import { ProfileScreen } from './screens/ProfileScreen'
 import { AdminScreen } from './screens/AdminScreen'
+import { LandingScreen } from './screens/LandingScreen'
 export const App = () => {
   return (
     <>
       <Navbar />
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<LandingScreen />} />
           <Route path="/home" element={<HomeScreen />} />
           <Route path="/book/:roomid/:checkin/:checkout" element={<BookingScreen />} />
-          <Route path='/login' element = {<LoginScreen />} />
-          <Route path='/register' element = {<RegisterScreen />} />
-          <Route path='/profile' element = { <ProfileScreen />} />
-          <Route path='/admin' element = { <AdminScreen />} />
+          <Route path='/login' element={<LoginScreen />} />
+          <Route path='/register' element={<RegisterScreen />} />
+          <Route path='/profile' element={<ProfileScreen />} />
+          <Route path='/admin' element={<AdminScreen />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/screens/LandingScreen.jsx
@@ -0,0 +1,13 @@
+export const LandingScreen = () => {
+    return (
+        <div className="row landing">
+            <div className="col-md-12">
+                <h1>Sheyrooms</h1>
+                <h3>There's only one boss, The Guest.</h3>
+                <a href='/home'>
+                    <button className="btn btn-primary">Get Started</button>
+                </a>
+            </div>
+        </div>
+    )
+}
